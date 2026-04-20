6043f01fa09b23168ecca93a1c38412de9559888
Jeremy Bourqui <jbourqui@gmail.com>
2026-04-20 13:09:57 +0200

    feat: add Matomo tracking script to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import Test from './pages/Test';
 import DepthDitherFilter from './components/SvgFilters/DepthDitherFilter';
 
 function App() {
+
+  React.useEffect(() => {
+   var _mtm = window._mtm = window._mtm || [];
+   _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+   g.async=true; g.src='https://cdn.matomo.cloud/jeremybourqui.matomo.cloud/container_W1WW5Olx.js'; s.parentNode.insertBefore(g,s);
+  }, [])
+
+
   return (
     <BrowserRouter>
       <DepthDitherFilter />
